3aa8afff6fd2c2ab552a21b2ad9e56f61adab741
Daniel <ifeoluwadaniel25@gmail.com>
2023-02-10 22:37:20 +0100

    Continued working on the bookmark website

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark)
 function saveBookmark(e){
 //Get form values
 var siteName = document.getElementById('siteName').value
-
 var siteUrl = document.getElementById('siteUrl').value
+
+if (!siteName || !siteUrl){
+    alert('Please fill in the form')
+    return false
+}
 
  var bookmark = {
     name: siteName,
@@ -30,11 +34,29 @@ if(localStorage.getItem('bookmarks') ===  null) {
    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
 }
 
+fetchBookmarks()
+
 
 //prevents form from submitting
     e.preventDefault();
 }
 
+//delete
+
+function deleteBookmark(url) {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+    //Loop through
+    for(var i = 0 ; i < bookmarks.length; i++ ){
+if(bookmarks[i].url == url){
+    bookmarks.splice(i,1)
+}
+    }
+
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+
+    fetchBookmarks()
+}
+ 
 //Fetch Bookmarks
 function fetchBookmarks(){
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
@@ -48,6 +70,9 @@ for(var i = 0; i < bookmarks.length; i++){
 var name = bookmarks[i].name;
 var url = bookmarks[i].url;
 
-bookmarksResults.innerHTML += '<div class="well">' + '<h3>'+name+ '<a class="btn btn-default" target="_blank" href="'+url+'" >Visit<a/>'+'</h3>'+'</div>' ;
+bookmarksResults.innerHTML += '<div class="well">' + '<h3>'+name+ 
+'<a class="btn btn-success" target="_blank" href="'+url+'" >Visit <a/>'
++ '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" >Delete</a>'
++'</h3>'+'</div>' ;
 }
 }
